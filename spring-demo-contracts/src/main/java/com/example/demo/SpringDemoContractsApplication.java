0b18ed14d9e290648187e9a42c48fb2133459efa
Andrey_Shleger <Andrey_Shleger@epam.com>
2019-10-06 18:50:20 +0300

    init spring contract project

---
+++ spring-demo-contracts/src/main/java/com/example/demo/SpringDemoContractsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoContractsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDemoContractsApplication.class, args);
+  }
+
+}
